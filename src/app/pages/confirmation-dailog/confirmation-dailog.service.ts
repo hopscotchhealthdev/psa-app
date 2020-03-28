1b6a386b7db79ff66c35bdbc465adcb799677cb1
Jayant <jayantpaliwal@gmail.com>
2020-03-28 11:44:34 +0530

    minor udates

--- src/app/pages/confirmation-dailog/confirmation-dailog.service.ts
+++ src/app/pages/confirmation-dailog/confirmation-dailog.service.ts
@@ -15,7 +15,8 @@ export class ConfirmationDailogService {
     btnOkText: string = 'Yes',
     btnCancelText: string = 'No',
     dialogSize: 'sm'|'lg' = 'sm'): Promise<boolean> {
-    const modalRef = this.modalService.open(ConfirmationDailogComponent, { size: dialogSize });
+    const modalRef = this.modalService.open(ConfirmationDailogComponent, { size: dialogSize,backdrop : 'static',
+    keyboard : false });
     modalRef.componentInstance.title = title;
     modalRef.componentInstance.message = message;
     modalRef.componentInstance.btnOkText = btnOkText;
