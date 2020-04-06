9b6ba06dfd9b840f7a9ad3bb7874673c3087395e
Jayant <jayantpaliwal@gmail.com>
2020-04-06 16:55:50 +0530

    fixed button issue

--- src/app/pages/psa-list/psa-list.component.ts
+++ src/app/pages/psa-list/psa-list.component.ts
@@ -13,12 +13,21 @@ export class PsaListComponent implements OnInit {
   psa = [];
   psaSelect: string = '';
   instruction = false;
+  src:string="";
   constructor(private router: Router, private route: ActivatedRoute) {
 
   }
   ngOnInit() {
     this.route.queryParamMap.subscribe(params => {
       if (params.get("instruction")) {
+        var userAgent = window.navigator.userAgent;
+        if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+          // iPad or iPhone 
+          this.src="https://firebasestorage.googleapis.com/v0/b/recordingmechanic.appspot.com/o/instruction%2FFile%20from%20iOS.MP4?alt=media&token=c0dcafda-4d33-46e9-8a1d-f4ca6af1d1dd"
+        }
+        else {
+       this.src="https://firebasestorage.googleapis.com/v0/b/recordingmechanic.appspot.com/o/instruction%2FFile%20from%20iOS%20(3).mp4?alt=media&token=e3d47df5-b926-4f8c-a601-3a5c0aac8b93"
+        }
         this.instruction = true;
       }
     })
