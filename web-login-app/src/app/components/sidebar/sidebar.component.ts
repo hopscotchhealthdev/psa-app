b70c621dc998f02a0526c1037504fbc2c52b4021
Jayant <jayantpaliwal@gmail.com>
2020-05-12 19:04:24 +0530

    Merge Login APP changes

---
+++ web-login-app/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from "@angular/core";
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  {
+    path: "/home",
+    title: "HomePage",
+    icon: "icon-chart-pie-36",
+    class: ""
+  },
+  {
+    path: "/video-recorder",
+    title: "Video Recoder",
+    icon: "icon-pin",
+    class: ""
+  },
+  {
+    path: "/profile",
+    title: "Profile",
+    icon: "icon-single-02",
+    class: ""
+  },
+  
+];
+
+@Component({
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"]
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+    if (window.innerWidth > 991) {
+      return false;
+    }
+    return true;
+  }
+}
